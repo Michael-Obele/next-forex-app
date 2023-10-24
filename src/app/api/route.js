f5ae6a2d90265c73b49587f065a49e464871f8c3
Michael Obubelebra Amachree <amachree9630@gmail.com>
2023-10-24 18:12:37 +0100

    Bug Fix: Set queryTime to true

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -2,7 +2,9 @@ import Ably from 'ably/promises';
 import { NextResponse } from 'next/server';
 
 export async function GET(request) {
-  const client = new Ably.Realtime(process.env.API_KEY);
+  const client = new Ably.Realtime(process.env.API_KEY, {
+    queryTime: true,
+  });
   const tokenRequestData = await client.auth.createTokenRequest({
     clientId: `anonymous-${Math.random().toString(36).substring(8)}`,
   });
