c6fa2d09fb50befb98c39276d36cb2a732b74d44
Michael Obubelebra Amachree <amachree9630@gmail.com>
2023-10-24 17:47:19 +0100

    Minor Update: Edit to clientID

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -3,7 +3,7 @@ import Ably from 'ably/promises';
 export async function GET(request) {
   const client = new Ably.Realtime(process.env.API_KEY);
   const tokenRequestData = await client.auth.createTokenRequest({
-    clientId: `anonymous-${Math.random().toString(36).substring(7)}`,
+    clientId: `anonymous-${Math.random().toString(36).substring(8)}`,
   });
   return Response.json(tokenRequestData);
 }
