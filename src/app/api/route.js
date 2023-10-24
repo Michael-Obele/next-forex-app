c9ad2b63c30c340349aebd3a8c4fadbc5c57614d
Michael Obubelebra Amachree <amachree9630@gmail.com>
2023-10-24 17:59:53 +0100

    Minor update: revalidate to 0

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,9 +1,10 @@
 import Ably from 'ably/promises';
+import { NextResponse } from 'next/server';
 
 export async function GET(request) {
   const client = new Ably.Realtime(process.env.API_KEY);
   const tokenRequestData = await client.auth.createTokenRequest({
     clientId: `anonymous-${Math.random().toString(36).substring(8)}`,
   });
-  return Response.json(tokenRequestData);
+  return NextResponse.json(tokenRequestData, { revalidate: 0 });
 }
